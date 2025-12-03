5751e0a64a4678f60078d1a537e462818ffeafc8
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-12-02 22:00:30 -0800

    legal 0

---
+++ apps/www/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import { PrivacyPolicyScreen } from "./legal";
+
+export default function PrivacyPolicyPage() {
+  return (
+    <PrivacyPolicyScreen
+      appUrl="https://manaflow.com"
+      company="Manaflow"
+      contactEmail="austin@manaflow.com"
+      lastUpdated="12/2/2025"
+    />
+  );
+}
---
+++ apps/www/terms-of-service/page.tsx
@@ -0,0 +1,11 @@
+import { TermsOfServiceScreen } from "../privacy-policy/legal";
+
+export default function TermsOfServicePage() {
+  return (
+    <TermsOfServiceScreen
+      appUrl="https://manaflow.com"
+      company="Manaflow"
+      contactEmail="austin@manaflow.com"
+    />
+  );
+}
